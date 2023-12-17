35b6795ebe5aeb0077e1826394b8652b59e9433b
subs <99681487+SubGlitch1@users.noreply.github.com>
2023-12-17 03:44:30 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# DentalXrayAI
- 
+# ToothXRayAI
+A computer vision project for AI teeth xray analysis. This repo features a yolov8 object detection model based on the [dentex dataset](https://universe.roboflow.com/dentex/dentex-3xe7e) and a simple web interface for querying it. 
+
+![Screenshot](images/img1.png)
+
+## Content
+1. Self hosting
+2. Training
+3. Evaluation
+
+# Self Hosting
+If you dont want to use the demo deployment and deploy the webUI yourself you have 2 options. You can either download the pretrained yolov8 model that i trained or you can train it yourself. After downloading it/training it just pop it into the root of this repo and name it "best.pt". After that run 
+```
+python3 -m pip install -r requirements.txt
+```
+followed by
+```
+python3 object_detector.py
+```
+This should make the UI available on http://127.0.0.1:8080
+## Training Proccess
+For training i used the YOLO package from the python ultralytics libary. Luckily this makes the training fairly easy.
+Step 1: Head over to roboflow and create an account. Once youre logged in go to https://universe.roboflow.com/dentex/dentex-3xe7e/dataset/2 and choose download dataset. Instead of downloading it directly i would recommend to get the download code in python and running that as the dataset has to be in ~/datasets/dentex-2/ in order for our training tool to find it. After the dir is in that location run 
+
+```
+pip3 install ultralytics roboflow && yolo train data=data.yaml model=yolov8n.pt epochs=50 imgsz=640 cache=True
+```
+The model will be saved in ~/datasets/dentex-2/runs/detect/train/weights/best.pt.
---
+++ object_detector.py
@@ -0,0 +1,60 @@
+from ultralytics import YOLO
+from flask import request, Flask, jsonify
+from waitress import serve
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    """
+    Site main page handler function.
+    :return: Content of index.html file
+    """
+    with open("templates/index.html") as file:
+        return file.read()
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    """
+        Handler of /detect POST endpoint
+        Receives uploaded file with a name "image_file", passes it
+        through YOLOv8 object detection network and returns and array
+        of bounding boxes.
+        :return: a JSON array of objects bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    buf = request.files["image_file"]
+    boxes = detect_objects_on_image(buf.stream)
+    return jsonify(boxes)
+
+
+def detect_objects_on_image(buf):
+    """
+    Function receives an image,
+    passes it through YOLOv8 neural network
+    and returns an array of detected objects
+    and their bounding boxes
+    :param buf: Input image file stream
+    :return: Array of bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    model = YOLO("best.pt")
+    results = model.predict(Image.open(buf))
+    result = results[0]
+    output = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = [
+            round(x) for x in box.xyxy[0].tolist()
+        ]
+        class_id = box.cls[0].item()
+        prob = round(box.conf[0].item(), 2)
+        prob_percentage = f"{prob * 100:.2f}%"
+        output.append([
+            x1, y1, x2, y2, result.names[class_id], prob_percentage
+        ])
+
+
+    return output
+
+
+serve(app, host='0.0.0.0', port=8080)
